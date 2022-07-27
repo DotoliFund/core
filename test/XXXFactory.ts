998509a1db88d498dbd5a4038731622485e92cdb
funddeveloper22 <openfund365@gmail.com>
2022-07-27 10:10:02 +0900

    import abi json file for hardhat test

--- test/XXXFactory.ts
+++ test/XXXFactory.ts
@@ -1,2 +1,5 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+
+import UniswapV2Factory from '../artifacts/contracts/XXXFactory.sol/XXXFactory.json';
+import UniswapV2Pair from '../artifacts/contracts/XXXFund.sol/XXXFund.json';
