f1930ae4906d1f863896e3d2dd61078b86be1d61
funddeveloper22 <openfund365@gmail.com>
2022-08-17 14:11:47 +0900

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,6 +30,10 @@ const config: HardhatUserConfig = {
     goerli: {
       url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [GOERLI_PRIVATE_KEY]
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
     }
   },
   namedAccounts: {
