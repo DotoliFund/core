bc55b549ee4d40f54b637b81b68212b9b2843ec9
fundDeveloper22 <openfund365@gmail.com>
2022-10-04 17:51:26 +0900

    add getPrice() function

--- contracts/XXXFund2.sol
+++ contracts/XXXFund2.sol
@@ -9,13 +9,15 @@ import './interfaces/IXXXFactory.sol';
 import './interfaces/IERC20.sol';
 import '@uniswap/v3-periphery/contracts/libraries/Path.sol';
 import '@uniswap/swap-router-contracts/contracts/interfaces/ISwapRouter02.sol';
-
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+import "@uniswap/v3-periphery/contracts/libraries/OracleLibrary.sol";
 
 import "hardhat/console.sol";
 
 contract XXXFund2 is IXXXFund2 {
     using Path for bytes;
 
+    address UNISWAP_V3_FACTORY = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
     address WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     //address WETH9 = 0xc778417E063141139Fce010982780140Aa0cD5Ab;
 
@@ -492,5 +494,69 @@ contract XXXFund2 is IXXXFund2 {
                 }
             }
         }
+    }
+
+    function getPrice(
+        address _token0, 
+        address _token1, 
+        uint24 _fee,
+        address tokenIn,
+        uint128 amountIn,
+        uint32 secondsAgo
+    ) external view returns (uint256 amountOut) {
+        address token0 = _token0;
+        address token1 = _token1;
+        uint24 fee = _fee;
+
+        address pool = IUniswapV3Factory(UNISWAP_V3_FACTORY).getPool(
+            _token0,
+            _token1,
+            _fee
+        );
+        require(pool != address(0), "pool doesn't exist");
+
+        require(tokenIn == token0 || tokenIn == token1, "invalid token");
+
+        address tokenOut = tokenIn == token0 ? token1 : token0;
+
+        // (int24 tick, ) = OracleLibrary.consult(pool, secondsAgo);
+
+        // Code copied from OracleLibrary.sol, consult()
+        uint32[] memory secondsAgos = new uint32[](2);
+        secondsAgos[0] = secondsAgo;
+        secondsAgos[1] = 0;
+
+        // int56 since tick * time = int24 * uint32
+        // 56 = 24 + 32
+        (int56[] memory tickCumulatives, ) = IUniswapV3Pool(pool).observe(
+            secondsAgos
+        );
+
+        int56 tickCumulativesDelta = tickCumulatives[1] - tickCumulatives[0];
+
+        // int56 / uint32 = int24
+        int24 tick = int24(tickCumulativesDelta / secondsAgo);
+        // Always round to negative infinity
+        /*
+        int doesn't round down when it is negative
+        int56 a = -3
+        -3 / 10 = -3.3333... so round down to -4
+        but we get
+        a / 10 = -3
+        so if tickCumulativeDelta < 0 and division has remainder, then round
+        down
+        */
+        if (
+            tickCumulativesDelta < 0 && (tickCumulativesDelta % secondsAgo != 0)
+        ) {
+            tick--;
+        }
+
+        amountOut = OracleLibrary.getQuoteAtTick(
+            tick,
+            amountIn,
+            tokenIn,
+            tokenOut
+        );
     }
 }
