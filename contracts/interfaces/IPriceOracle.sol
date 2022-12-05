b42ca1d41e9abd82ec3ed8704e86febbe3c305c2
fundDeveloper22 <openfund365@gmail.com>
2022-12-06 07:22:19 +0900

    remove price oracle

--- contracts/interfaces/IPriceOracle.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-// Inspired by Uniswap
-pragma solidity =0.7.6;
-pragma abicoder v2;
-
-interface IPriceOracle {
-    function getPriceETH(address token, uint128 amountIn, address weth) external view returns (uint256);
-    function getPriceUSD(address token, uint128 amountIn, address usd) external view returns (uint256);
-}
