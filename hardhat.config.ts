34c1d86e691281c3b2b8c1b5c5c00fe0bd26c660
fundDeveloper22 <openfund365@gmail.com>
2023-01-06 11:16:22 +0900

    change to alchemy, add polygon network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,9 @@ import "hardhat-deploy";
 require("@nomicfoundation/hardhat-chai-matchers")
 require('dotenv').config()
 
-const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const API_URL_MAINNET = process.env.API_URL_MAINNET;
+const API_URL_GOERLI = process.env.API_URL_GOERLI;
+const API_URL_POLYGON_MAINNET = process.env.API_URL_POLYGON_MAINNET;
 
 // Replace this private key with your Goerli account private key
 // To export your private key from Metamask, open Metamask and
@@ -24,6 +26,33 @@ if (typeof TEST_PRIVATE_KEY3 === 'undefined') {
   throw new Error(`TEST_PRIVATE_KEY3 must be a defined environment variable`);
 }
 
+//============================================ check wallet nounce ==========================================
+
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const { task } = require("hardhat/config");
+
+task("account", "returns nonce and balance for specified address on multiple networks")
+  .addParam("address")
+  .setAction(async address => {
+    const web3Mainnet = createAlchemyWeb3(`https://eth-mainnet.g.alchemy.com/v2/${API_URL_MAINNET}`);
+    const web3Goerli = createAlchemyWeb3(`https://eth-goerli.g.alchemy.com/v2/${API_URL_GOERLI}`);
+    const web3Polygon = createAlchemyWeb3(`https://polygon-mainnet.g.alchemy.com/v2/${API_URL_POLYGON_MAINNET}`);
+
+    const networkIDArr = ["Ethereum Mainnet:", "Ethereum Goerli:", "Polygon :"]
+    const providerArr = [web3Mainnet, web3Goerli, web3Polygon];
+    const resultArr = [];
+    
+    for (let i = 0; i < providerArr.length; i++) {
+      const nonce = await providerArr[i].eth.getTransactionCount(address.address, "latest");
+      const balance = await providerArr[i].eth.getBalance(address.address)
+      resultArr.push([networkIDArr[i], nonce, parseFloat(providerArr[i].utils.fromWei(balance, "ether")).toFixed(2) + "ETH"]);
+    }
+    resultArr.unshift(["  |NETWORK|   |NONCE|   |BALANCE|  "])
+    console.log(resultArr);
+  });
+
+//===========================================================================================================
+
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [
@@ -39,21 +68,25 @@ const config: HardhatUserConfig = {
     ],
   },
   networks: {
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [TEST_PRIVATE_KEY1]
-    },
-    goerli: {
-      url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [TEST_PRIVATE_KEY1, TEST_PRIVATE_KEY2, TEST_PRIVATE_KEY3]
-    },
     hardhat: {
       chainId: 31337,
       forking: {
-        url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
+        url: `https://eth-mainnet.alchemyapi.io/v2/${API_URL_MAINNET}`,
         //blockNumber: 14390000
       },
       //allowUnlimitedContractSize: true,
+    },
+    mainnet: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${API_URL_MAINNET}`,
+      accounts: [TEST_PRIVATE_KEY1]
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${API_URL_GOERLI}`,
+      accounts: [TEST_PRIVATE_KEY1, TEST_PRIVATE_KEY2, TEST_PRIVATE_KEY3]
+    },
+    polygon: {
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${API_URL_POLYGON_MAINNET}`,
+      accounts: [TEST_PRIVATE_KEY1, TEST_PRIVATE_KEY2, TEST_PRIVATE_KEY3]
     },
   },
   namedAccounts: {
